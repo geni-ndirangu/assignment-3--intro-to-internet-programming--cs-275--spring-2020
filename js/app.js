29eb90e67afa1fc1041474e046717cef991dc09d
Ivy <ndirangu.ivy@mail.com>
2020-05-12 23:46:14 -0400

    Add hover element to table data

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
+const cells = document.querySelectorAll(`td`);
+let lastRotatedElement;
 
+function rotateCell(){
+    const rotated = this.classList.contains(`rotate`);
+    if (!rotated){
+        this.classList.add(`rotate`);
+        if (lastRotatedElement) lastRotatedElement.classList.remove(`rotate`);
+        lastRotatedElement = this;
+    } else {
+        this.classList.remove(`rotate`);
+        lastRotatedElement = null;
+    }
+}
+
+cells.forEach(cell => cell.addEventListener(`click`, rotateCell));
